c1bdbca181a74998938f89fd018c04e29bc41d1a
agrevel <166759040+agrevel@users.noreply.github.com>
2024-04-11 17:25:04 -0400

    Add files via upload

--- Image.java
+++ Image.java
@@ -1,15 +1,46 @@
+import java.awt.*;
+
 abstract class Image {
-
-    public Image(int width, int height) {
+    //abstract Image class, which stores an image width and height as instance variables.
+    private int width;
+    private int height;
+    //Non-final arrays
+    private Color[][] colors;
+    Image(int width, int height) {
+        //Image will receive width and height as parameter
+        this.setWidth(width);
+        this.setHeight(height);
+        //Instantiate the color array with width and height
+        this.colors = new Color[height][width];
+    }
+    Image() {
+        //Assign w/h to zero. DO NOT INSTANTIATE THE COLOR ARRAY!
+        this.width = 0;
+        this.height = 0;
     }
 
-    public String getHeight() {
+    //6 accessor methods, 3 getters and 3 setters.
+
+    //Side note: 90% sure the color had to be one of them but if I'm wrong let me know.
+    public int getWidth() {
+        return width;
     }
 
-    public String getWidth() {
+    public void setWidth(int width) {
+        this.width = width;
     }
 
-    public void setPixel(int x, int y, Color color) {
+    public int getHeight() {
+        return height;
     }
 
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public Color[][] getColors() {
+        return colors;
+    }
+    public void setColors(Color[][] colors) {
+        this.colors = colors;
+    }
 }
