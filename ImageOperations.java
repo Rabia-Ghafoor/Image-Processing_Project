3781bd53d4fa14d008d2cd537f1434e1b774aa18
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2024-04-23 17:13:47 -0400

    rabia's changes to image operations.java

--- ImageOperations.java
+++ ImageOperations.java
@@ -5,42 +5,56 @@ class ImageOperations {
   public static Image zeroRed(Image img){
 
       for (int y = 0; y < img.getHeight(); y++) {
+
           for (int x = 0; x < img.getWidth(); x++) {
+
+              // get the color of the pixel at position x, y
               Color pixel = new Color(img.getRGB(x, y));
+
+                // set the red value of the pixel to 0
+
               int newRed = 0;
+
+              // get the green, blue, and alpha values of the pixel
               int green = pixel.getGreen();
               int blue = pixel.getBlue();
               int alpha = pixel.getAlpha();
+
+                // create a new color object with the new red value and the original green, blue, and alpha vals
               int rgb = (alpha << 24) | (newRed << 16) | (green << 8) | blue;
+
+                 // set the color of the pixel at position x, y to the new color
               img.setRGB(x, y, rgb);
           }
       }
       return img;
 
-
-
     }
 
-
     public static Image grayscale(Image img){
-        for (int y = 0; y < img.getHeight(); y++) {
-            for (int x = 0; x < img.getWidth(); x++) {
+
+        for (int y = 0; y < img.getHeight(); y++) { //looping through the height of the image
+
+            for (int x = 0; x < img.getWidth(); x++) { //looping through the width of the image
+              //get the color of the pixel at position x, y
                 Color pixel = new Color(img.getRGB(x, y));
+                //calculate the average of the red, green, and blue values of the pixel
                 int gray = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+                //create a new color object with the average value
                 int rgb = (pixel.getAlpha() << 24) | (gray << 16) | (gray << 8) | gray;
+                //set the color of the pixel at position x, y to the new color
                 img.setRGB(x, y, rgb);
             }
         }
+
         return img;
 
     }
 
-
-
     public static Image invert(Image img){
 
-        for (int y = 0; y < img.getHeight(); y++) {
-            for (int x = 0; x < img.getWidth(); x++) {
+        for (int y = 0; y < img.getHeight(); y++) { //looping through the height of the image
+            for (int x = 0; x < img.getWidth(); x++) { //looping through the width of the image
                 Color pixel = new Color(img.getRGB(x, y));
                 int red = 255 - pixel.getRed();
                 int green = 255 - pixel.getGreen();
@@ -54,16 +68,24 @@ class ImageOperations {
 
     }
 
+
+
     public static Image crop(Image, int x1, int y1, int w, int h){
 
-      return img.getSubimage(x1, y1, w, h);
-
+        Image myImage = new Image(w, h);
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                Color pixel = new Color(img.getRGB(x1 + x, y1 + y));
+              myImage.setRGB(x, y, pixel.getRGB());
+            }
+        }
+        return myImage;
 
     }
 
     public static Image mirror(Image img, String mode){
 
-        if (mode.equals("horizontal")) {
+        if (mode.equals("Horizontal")) {
             for (int y = 0; y < img.getHeight(); y++) {
                 for (int x = 0; x < img.getWidth() / 2; x++) {
                     Color left = new Color(img.getRGB(x, y));
@@ -72,7 +94,7 @@ class ImageOperations {
                     img.setRGB(img.getWidth() - x - 1, y, left.getRGB());
                 }
             }
-        } else if (mode.equals("vertical")) {
+        } else if (mode.equals("Vertical")) {
             for (int y = 0; y < img.getHeight() / 2; y++) {
                 for (int x = 0; x < img.getWidth(); x++) {
                     Color top = new Color(img.getRGB(x, y));
@@ -85,6 +107,12 @@ class ImageOperations {
         return img;
 
     }
+
+
+
+
+
+
 
     public static  Image repeat(Image img, int n, String dir){
 
